668a9b9582c51d06dd54dd893ddb4d8b9dcf86c6
Athryx <jack.x.roscoe@gmail.com>
2022-03-05 17:52:38 -0600

    bugfix in remote_viewing

--- vision/src/remote_viewing.cpp
+++ vision/src/remote_viewing.cpp
@@ -2,16 +2,16 @@
 #include "gst/gstelement.h"
 #include "util.h"
 #include <assert.h>
+#include <string>
 
 #include <iostream>
-#include <string>
 
 RemoteViewing::RemoteViewing(const std::string& host, u16 port, int width, int height) {
 	// TODO: set these properties without using parse_luanch
 	// Read the video in at 1920x1080 and then scale it, becuase rading it in at a lower resolution
 	// will reduce the field of view on the raspberry pi camera that we have
 	// TODO: make the resolution we read in configurable
-	auto pipeline_description = std::string("v4l2src device=\"/dev/video0\" ! video/x-raw,width=1920,height=1080"
+	auto pipeline_description = std::string("v4l2src device=\"/dev/video0\""
 		" ! videoscale ! video/x-raw,width=" + std::to_string(width) + ",height=" + std::to_string(height) +
 		" ! videoscale ! videoconvert ! x264enc tune=zerolatency bitrate=500 speed-preset=superfast"
 		" ! rtph264pay config-interval=10 pt=96 ! udpsink host=") + host + std::string(" port=") + std::to_string(port);
