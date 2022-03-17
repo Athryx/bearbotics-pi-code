673df1885ea2e1d27c5add4713437bcd928c14c9
Athryx <jack.x.roscoe@gmail.com>
2022-03-16 23:08:03 -0500

    hsv viewer now works with multiple images

--- hsv-viewer/main.cpp
+++ hsv-viewer/main.cpp
@@ -1,32 +1,69 @@
-#include "argparse.hpp"
 #include <cstdlib>
 #include <iostream>
+#include <mutex>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 #include <stdexcept>
 #include <stdio.h>
+#include <unordered_map>
 #include <vector>
+#include <string>
+#include <string.h>
 #include <algorithm>
 
-std::string window_name("hsv-viewer");
-cv::Mat src;
+struct HsvVals {
+	int hmin { 0 };
+	int smin { 0 };
+	int vmin { 0 };
 
-std::vector<cv::Point> points;
+	int hmax { 0 };
+	int smax { 0 };
+	int vmax { 0 };
+};
 
-int hmin = 180;
-int smin = 255;
-int vmin = 255;
+struct HsvPoint {
+	cv::Point point;
+	int h;
+	int s;
+	int v;
+};
 
-int hmax = 0;
-int smax = 0;
-int vmax = 0;
+struct WindowData {
+	WindowData(cv::Mat&& img):
+	img(std::move(img)) {}
 
-static void onMouse(int e, int x, int y, int f, void *unused) {
+	WindowData(WindowData&& data) = default;
+
+	cv::Mat img;
+	std::vector<HsvPoint> points {};
+};
+
+std::mutex windows_lock;
+std::unordered_map<std::string, WindowData> windows;
+
+std::mutex vals_lock;
+HsvVals vals = HsvVals {
+	.hmin = 256,
+	.smin = 256,
+	.vmin = 256,
+	.hmax = 0,
+	.smax = 0,
+	.vmax = 0,
+};
+
+// TODO: add a way of undoing clicks
+static void onMouse(int e, int x, int y, int f, void *in_window_name) {
+	std::string window_name((char *) in_window_name);
+
 	cv::MouseEventTypes event = (cv::MouseEventTypes) e;
 	cv::MouseEventFlags flags = (cv::MouseEventFlags) f;
 
-	cv::Mat image = src.clone();
+	std::scoped_lock<std::mutex> lock(windows_lock);
+
+	auto& window_data = windows.at(window_name);
+
+	cv::Mat image = window_data.img.clone();
 
 	cv::Mat hsv_img;
 	cv::Mat rgb_img = image(cv::Rect(x, y, 1, 1));
@@ -43,25 +80,37 @@ static void onMouse(int e, int x, int y, int f, void *unused) {
 	int v = hsv.val[2];
 
 	if (event == cv::EVENT_LBUTTONDOWN) {
-		points.push_back(cv::Point(x, y));
+		auto point = HsvPoint {
+			.point = cv::Point(x, y),
+			.h = h,
+			.s = s,
+			.v = v,
+		};
 
-		hmin = std::min(hmin, h);
-		smin = std::min(smin, s);
-		vmin = std::min(vmin, v);
+		window_data.points.push_back(point);
 
-		hmax = std::max(hmax, h);
-		smax = std::max(smax, s);
-		vmax = std::max(vmax, v);
+		{
+			std::scoped_lock<std::mutex> lock2(vals_lock);
 
-		printf("New set of bounds\n");
-		printf("min: H: %d, S: %d, V: %d\n", hmin, smin, vmin);
-		printf("max: H: %d, S: %d, V: %d\n", hmax, smax, vmax);
+			vals.hmin = std::min(vals.hmin, h);
+			vals.smin = std::min(vals.smin, s);
+			vals.vmin = std::min(vals.vmin, v);
+	
+			vals.hmax = std::max(vals.hmax, h);
+			vals.smax = std::max(vals.smax, s);
+			vals.vmax = std::max(vals.vmax, v);
+
+			// TODO: only print if min or max actually changes
+			printf("New set of bounds\n");
+			printf("min: H: %d, S: %d, V: %d\n", vals.hmin, vals.smin, vals.vmin);
+			printf("max: H: %d, S: %d, V: %d\n", vals.hmax, vals.smax, vals.vmax);
+		}
 	}
 
 	auto font = cv::FONT_HERSHEY_SIMPLEX;
 
-	for (auto point : points) {
-		cv::circle(image, point, 5, cv::Scalar(255, 0, 0), cv::FILLED);
+	for (auto& point : window_data.points) {
+		cv::circle(image, point.point, 5, cv::Scalar(255, 0, 0), cv::FILLED);
 	}
 
 	char name[30];
@@ -92,24 +141,43 @@ static void onMouse(int e, int x, int y, int f, void *unused) {
 	imshow(window_name, image);
 }
 
+void usage() {
+	printf("Usage: hsv-viewer [options] [images]...\n\n"
+		"Positional Arguments:\n"
+		"images		list of 8 bit rgb images to to view in the hsv viewer\n\n"
+		"Optional Arguments:\n"
+		"-h --help	shows help message and exits\n");
+}
+
 int main(int argc, char **argv) {
-	argparse::ArgumentParser program("hsv-viewew", "0.0.1");
-
-	program.add_argument("image")
-		.help("8 bit rgb image to view in the hsv viewer");
-
-	try {
-		program.parse_args(argc, argv);
-	} catch (const std::runtime_error& err) {
-		std::cout << err.what() << std::endl;
-		std::cout << program;
+	if (argc <= 1) {
+		printf("must provide at least 1 argument\n");
+		usage();
 		exit(1);
 	}
 
-	auto image = program.get("image");
+	if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+		usage();
+		exit(0);
+	}
 
-	src = cv::imread(image, 1);
-	imshow(window_name, src);
-	cv::setMouseCallback(window_name, onMouse, 0);
+
+	windows_lock.lock();
+
+	for (int i = 1; i < argc; i ++) {
+		std::string arg(argv[i]);
+
+		if (windows.contains(arg)) {
+			continue;
+		}
+
+		auto img = cv::imread(arg, 1);
+		cv::imshow(arg, img);
+		windows.insert({arg, WindowData(std::move(img))});
+		cv::setMouseCallback(arg, onMouse, argv[i]);
+	}
+
+	windows_lock.unlock();
+
 	cv::waitKey();
 }
