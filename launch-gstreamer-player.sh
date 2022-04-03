1d6392b4ae2b4085085f0ffe0b57d7bd4208c19e
jack <jack.x.roscoe@gmail.com>
2022-04-03 13:04:43 -0500

    changed vision executable name from Vision to vision

--- launch-gstreamer-player.sh
+++ launch-gstreamer-player.sh
@@ -2,6 +2,7 @@
 
 while true
 do
-	gst-launch-1.0 rtspsrc location=rtsp://$1:5000/remote-viewing latency=0 ! rtph264depay ! decodebin ! videoconvert ! autovideosink
-	sleep 3
+	# set the timeout to be low so that when remote viewing is disabled (when switching to vision) the gstreamer window quickly closes
+	gst-launch-1.0 rtspsrc location=rtsp://$1:5000/remote-viewing latency=0 timeout=1000000 tcp-timeout=1500000 ! rtph264depay ! decodebin ! videoconvert ! autovideosink
+	sleep 2
 done
--- vision/start-vision.sh
+++ vision/start-vision.sh
@@ -3,5 +3,5 @@
 cd $(dirname $0)
 
 # For now, only run remote viewing without vision or mqtt
-exec ./src/build/Vision -r --image-width 640 --image-height 480 --fps 30 -m localhost templates/
-#exec ./src/build/Vision -r --image-width 640 --image-height 480 --fps 30 templates/
+exec ./src/build/vision -r --image-width 640 --image-height 480 --fps 30 -m localhost templates/
+#exec ./src/build/vision -r --image-width 640 --image-height 480 --fps 30 templates/
