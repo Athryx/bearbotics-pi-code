8c18583aaf94d3471e839477f7d4cec9f2f2a40c
jack <jack.x.roscoe@gmail.com>
2022-04-04 20:43:46 -0500

    Changed rtsp port from 5000 to 5800 (required by first)

--- launch-gstreamer-player.sh
+++ launch-gstreamer-player.sh
@@ -3,6 +3,6 @@
 while true
 do
 	# set the timeout to be low so that when remote viewing is disabled (when switching to vision) the gstreamer window quickly closes
-	gst-launch-1.0 rtspsrc location=rtsp://$1:5000/remote-viewing latency=0 timeout=1000000 tcp-timeout=1500000 ! rtph264depay ! decodebin ! videoconvert ! autovideosink
+	gst-launch-1.0 rtspsrc location=rtsp://$1:5800/remote-viewing latency=0 timeout=1000000 tcp-timeout=1500000 ! rtph264depay ! decodebin ! videoconvert ! autovideosink
 	sleep 2
 done
--- vision/start-vision.sh
+++ vision/start-vision.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)
 
 # For now, only run remote viewing without vision or mqtt
 #exec ./src/build/vision -r --image-width 640 --image-height 480 --fps 30 -m localhost templates/
-exec ./src/build/vision -r --image-width 640 --image-height 480 --fps 30 templates/
+exec ./src/build/vision -r --image-width 640 --image-height 480 --fps 30 --rtsp-port 5800 templates/
 
 # remote viewing on pi
-#exec ./src/build/vision -r --cam-width 1280 --cam-height 720 --image-width 320 --image-height 240 --fps 30 templates/
+#exec ./src/build/vision -r --cam-width 1280 --cam-height 720 --image-width 320 --image-height 240 --fps 30 --rtsp-port 5800 templates/
